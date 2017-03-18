f56bfca48f4104d00a946891ef6457a5bfecc586
Boris Tayoursky <borik.hub@gmail.com>
2017-03-19 06:41:06 +0900

    finished template

--- gulpfile.js
+++ gulpfile.js
@@ -5,6 +5,7 @@ var nib         = require('nib');
 var cssmin = require('gulp-cssmin');
 var rename = require('gulp-rename');
 var htmlmin = require('gulp-htmlmin');
+var uglify = require('gulp-uglify');
 var browserSync = require('browser-sync')
 var reload      = browserSync.reload;
 
@@ -31,16 +32,27 @@ gulp.task('styles', function(){
 
 });
 gulp.task('cssmin', function () {
-    gulp.src('dev/css/main.css')
+    gulp.src('dev/assets/css/main.css')
         .pipe(cssmin())
         //.pipe(rename({suffix: '.min'}))
         .pipe(gulp.dest('build/assets/css'));
 });
 gulp.task('htmlmin', function() {
-  return gulp.src('dev/index.html')
+     gulp.src('dev/index.html')
     .pipe(htmlmin({collapseWhitespace: true}))
     //.pipe(rename({suffix: '.min'}))
     .pipe(gulp.dest('build'));
+});
+// билдинг яваскрипта
+gulp.task('jsmin', function () {
+    gulp.src('dev/js/main.js') //Найдем наш main файл
+        //.pipe(rigger()) //Прогоним через rigger
+        //.pipe(sourcemaps.init()) //Инициализируем sourcemap
+        .pipe(uglify()) //Сожмем наш js
+        //.pipe(sourcemaps.write()) //Пропишем карты
+        //.pipe(rename({suffix: '.min'})) //добавим суффикс .min к выходному файлу
+        .pipe(gulp.dest('build/assets/js')); //выгрузим готовый файл в build
+        //.pipe(connect.reload()) //И перезагрузим сервер
 });
 
 gulp.task('html', function(){
@@ -64,9 +76,10 @@ gulp.task('browserSync', function() {
 });*/
 
 gulp.task('watch', function(){
-  gulp.watch('build/assets/css/main.css', ['cssmin'])
   gulp.watch('build/index.html', ['htmlmin'])
   gulp.watch('dev/js/*.js', ['js'])
   gulp.watch('dev/index.html', ['html']);
 });
 gulp.task('default', ['serve','watch']);//, 'browserSync'
+gulp.task('build', ['cssmin','htmlmin','jsmin']);//, build
+
